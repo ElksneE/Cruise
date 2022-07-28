49274b53944d27959ad9fcf11dc530b694b0314c
Елена <el_el_@mail.ru>
2022-07-29 00:19:59 +0300

    проверка3

--- source/js/main.js
+++ source/js/main.js
@@ -1,30 +1,19 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
-
+import {burgerMenu} from './script';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
   // ---------------------------------
-
   iosVhFix();
-
   // Modules
   // ---------------------------------
-
+  burgerMenu();
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
   });
-});
-
-const wrapperButton = document.querySelector('.header__toggle-wrapper');
-const button = document.querySelector('.header__toggle');
-const nav = document.querySelector('.header__nav');
-
-wrapperButton.addEventListener('click', function () {
-  button.classList.toggle('header__toggle--active');
-  nav.classList.toggle('header__nav--active');
 });
--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,14 @@
-const wrapperButton = document.querySelector('.header__toggle-wrapper');
-const button = document.querySelector('.header__toggle');
-const nav = document.querySelector('.header__nav');
+const burgerMenu = () => {
+  const wrapperButton = document.querySelector('.header__toggle-wrapper');
+  const button = document.querySelector('.header__toggle');
+  const nav = document.querySelector('.header__nav');
+  button.classList.remove('header__toggle--active');
+  nav.classList.remove('header__nav--active');
 
-wrapperButton .addEventListener('click', function () {
-  button.classList.toggle('header__toggle--active');
-  nav.classList.toggle('header__nav--active');
-});
+  wrapperButton.addEventListener('click', function () {
+    button.classList.toggle('header__toggle--active');
+    nav.classList.toggle('header__nav--active');
+  });
+};
+
+export {burgerMenu};
